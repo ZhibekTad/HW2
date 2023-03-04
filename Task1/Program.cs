2a42bf1e090707084146c7a6e8b8c5a894903e49
Zhibek Tadzhibaeva <jibektad@mail.ru>
2023-03-04 23:44:30 +0600

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number <100 || number >=1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+    return;
+} else
+{
+    int secondRank = number / 10 % 10;
+    Console.Write("вторая цифра -  " + secondRank);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 6:
+    {
+    Console.WriteLine("Yes");
+    break;
+    }
+    case 7:   
+    {
+    Console.WriteLine("Yes");
+    break;
+    }
+    default:
+    {
+    Console.WriteLine("No");
+    break;
+    }
+}
